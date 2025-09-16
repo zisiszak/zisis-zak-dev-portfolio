1660062ac51cff6b201b9de36ec574c33f52ccc1
zisiszak <126941721+zisiszak@users.noreply.github.com>
2025-09-16 18:49:07 +1000

    feat: hero image and style cleanup

--- src/components/project-link.tsx
+++ src/components/project-link.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'gatsby';
+import { GatsbyImage, type IGatsbyImageData } from 'gatsby-plugin-image';
 import * as React from 'react';
 
 interface Props {
@@ -8,22 +9,37 @@ interface Props {
 	slug: string;
 	excerpt: string;
 	repositoryUrl?: string | null;
+	heroImageAlt?: string | null;
+	heroImage?: IGatsbyImageData | null;
 }
 
-export const ProjectLink = ({ title, slug, excerpt, repositoryUrl }: Props) => {
+export const ProjectLink = ({
+	title,
+	slug,
+	excerpt,
+	heroImage,
+	heroImageAlt,
+}: Props) => {
 	return (
 		<article>
 			<Link
 				to={`/projects${slug}`}
-				className="group px-6 py-4 lg:px-8 lg:py-6 border-2 dark:border-amber-50 border-neutral-900 flex flex-row gap-4 justify-between hover:border-red-600 dark:hover:border-red-400 active:opacity-40"
+				className="group px-6 py-4 md:px-8 md:py-6 border-2 dark:border-amber-50 border-neutral-900 flex flex-col md:flex-row md:gap-8 gap-6 justify-between hover:border-red-600 dark:hover:border-red-400 active:opacity-40"
 				title="View project"
 			>
-				<div>
-					<h2 className="font-bold lg:text-xl text-lg">{title}</h2>
-					<p className="lg:text-base text-sm opacity-75 lg:line-clamp-1 line-clamp-2">
+				<div className="md:min-w-64">
+					<h2 className="font-bold md:text-xl text-lg">{title}</h2>
+					<p className="md:text-base text-sm opacity-75 md:line-clamp-none line-clamp-2">
 						{excerpt}
 					</p>
 				</div>
+				{heroImage && (
+					<GatsbyImage
+						image={heroImage}
+						alt={heroImageAlt!}
+						className="w-full h-full md:max-w-48 object-cover object-center max-h-40 md:max-h-48 md:aspect-square overflow-hidden"
+					/>
+				)}
 			</Link>
 		</article>
 	);
